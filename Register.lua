700bd52f4646552a9dc12bc4092d5f8f6846b65b
CJ Miller <github@cjm721.com>
2021-10-23 19:24:32 -0700

    Add some Hunter Sounds

--- Register.lua
+++ Register.lua
@@ -1,3 +1,5 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
 LSM:Register("sound", "|cffa22fc8Draxian There's Balls|r", [[Interface\Addons\CJMedia\Sounds\balls.ogg]]) 
+LSM:Register("sound", "|cffa9d271Silithid I Died|r ", [[Interface\Addons\CJMedia\Sounds\silithid_i_died.ogg]]) 
+LSM:Register("sound", "|cffa9d271Silithid This is Easy|r ", [[Interface\Addons\CJMedia\Sounds\silithid_this_is_easy.ogg]]) 
